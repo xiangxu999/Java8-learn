37d5271ea9367fe6b981473aed142ae0ce05842a
xiangxu999 <846212939@qq.com>
2021-09-24 09:45:48 +0800

    初始化项目

---
+++ src/com/xu/Java8-learn/chapter01/HelloWord.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+/**
+ * @Description:
+ * @PACKAGE_NAME:chapter01
+ * @author:旭日
+ * @data:2021/9/24 9:41
+ */
+public class HelloWord
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1111");
+    }
+}
